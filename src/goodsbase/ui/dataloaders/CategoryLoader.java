e34806d702fb7e1c175d90774674e74dd1aa362f
samgrim <ds.lagrim@gmail.com>
2015-07-16 10:29:10 +0300

    added category loader

---
+++ src/goodsbase/ui/dataloaders/CategoryLoader.java
@@ -0,0 +1,14 @@
+package goodsbase.ui.dataloaders;
+
+import javax.swing.tree.TreeNode;
+
+import goodsbase.ui.dataloaders.interfaces.CategoryTreeLoader;
+
+public class CategoryLoader implements CategoryTreeLoader {
+
+	@Override
+	public TreeNode getCatTree() {
+		return null;
+	}
+
+}
---
+++ src/goodsbase/ui/dataloaders/interfaces/CategoryTreeLoader.java
@@ -0,0 +1,11 @@
+package goodsbase.ui.dataloaders.interfaces;
+
+import javax.swing.tree.TreeNode;
+/**
+ * Loads tree of categories
+ */
+public interface CategoryTreeLoader {
+	
+	TreeNode getCatTree();
+
+}
