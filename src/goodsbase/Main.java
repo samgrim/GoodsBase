31919cb37f3538eaeaa3970b3b4a416526789b5e
samgrim <ds.lagrim@gmail.com>
2015-07-21 12:56:41 +0300

    modif. app launch

--- src/goodsbase/Main.java
+++ src/goodsbase/Main.java
@@ -9,37 +9,55 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
-
+	
+	private static final String loggingProps = "/logging.properties";
+	
+	private static final Logger log = Logger.getLogger(Main.class.getName());
+	
 	public static void main(String[] args) throws Exception, IOException {
-		URL url = Main.class.getResource("logging.properties");
-	
-		FileInputStream fin = new FileInputStream("logging.properties");
-		LogManager.getLogManager().readConfiguration(fin);
+		initLogger();
+		setLookAndFeel();
 		
-		
-		//LogManager.getLogManager().readConfiguration(
-		//		Main.class.getResourceAsStream("/logging.properties"));
-	
-		CategoryTree.main(args);
-		
-		/*try {
-			UIManager.setLookAndFeel(
-			        UIManager.getSystemLookAndFeelClassName());
+		try {	
 			DbConnection.init();
 		} catch (Exception e) {
-			e.printStackTrace(System.err);
+			log.log(Level.SEVERE, "Failed to start application", e);
 			JOptionPane.showMessageDialog(null, "Failed to launch application.\n" 
 					+ e.getMessage(), "Launch failed", JOptionPane.ERROR_MESSAGE);
 			System.exit(1);
 		}
-		MainWindow.launch();*/
 		
+		MainWindow.launch();		
 	}
+	
+	private static void initLogger(){
+		try {
+			LogManager.getLogManager().readConfiguration(
+					Main.class.getResourceAsStream(loggingProps));
+		} catch (SecurityException | IOException e) {
+			log.log(Level.WARNING, "Failed reading resource " + loggingProps , e);
+		}		
+	}
+	
+	private static void setLookAndFeel() {		
+		try {
+			UIManager.setLookAndFeel(
+			        UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			log.log(Level.WARNING, "Failed setting look and feel" + loggingProps , e);
+		}		
+	}
+	
+
 
 }
