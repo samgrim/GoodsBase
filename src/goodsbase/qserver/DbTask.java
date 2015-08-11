dac85bc7eb6a216eb238b7d1afb50509767375e1
samgrim <ds.lagrim@gmail.com>
2015-08-11 12:52:24 +0300

    add DbTask interface

---
+++ src/goodsbase/qserver/DbTask.java
@@ -0,0 +1,26 @@
+/**
+ * Contains classes for server that execute database queries
+ * for multiple clients.
+ */
+package goodsbase.qserver;
+
+import java.sql.ResultSet;
+
+/**
+ * Represents a task that contains execution of sql statements.
+ * @author Daria
+ */
+public interface DbTask {
+	
+	/**
+	 * Executes task
+	 */
+	void execute();
+	
+	/**
+	 * @return ResultSet that has been created during execute() 
+	 * or null if DbTask has not been executed yet or if execute()
+	 * has not created any ResultSet
+	 */
+	ResultSet getResult();
+}
