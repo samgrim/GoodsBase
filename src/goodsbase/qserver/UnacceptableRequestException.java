32b00caa0e25944c3b7025719c1fd010b317745a
samgrim <ds.lagrim@gmail.com>
2015-08-13 12:18:24 +0300

    add UnacceptableRequestException 	new file:   src/goodsbase/qserver/UnacceptableRequestException.java

---
+++ src/goodsbase/qserver/UnacceptableRequestException.java
@@ -0,0 +1,25 @@
+package goodsbase.qserver;
+
+/**To be thrown when QRequest doesn't suite to DBTask
+ * 
+ * @author Daria
+ *
+ */
+public class UnacceptableRequestException extends IllegalArgumentException {
+
+	public UnacceptableRequestException(String s) {
+		super(s);	
+	}
+
+	public UnacceptableRequestException(Throwable cause) {		
+		super(cause);
+	
+	}
+
+	public UnacceptableRequestException(String message, Throwable cause) {
+		super(message, cause);		
+	}
+	
+	private static final long serialVersionUID = -2445191333680914374L;
+
+}
