c11b605aa66ac86c352f12cd23ae6b608380a1ce
samgrim <ds.lagrim@gmail.com>
2015-08-17 17:43:04 +0300

    add loading from database 	src/goodsbase/model/Category.java

--- src/goodsbase/model/Category.java
+++ src/goodsbase/model/Category.java
@@ -1,5 +1,20 @@
 package goodsbase.model;
 
+import goodsbase.qserver.QRequest;
+import goodsbase.qserver.QServer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 /**Describes category of goods*/
 public class Category {
 	
@@ -9,7 +24,7 @@ public class Category {
 	private int parentId;
 	
 	/**@throws NullPointerException if name == null*/
-	public Category(int id, int parentId, String name, String description) {
+	private Category(int id, int parentId, String name, String description) {
 		if(name == null) throw new  NullPointerException("Name can't be null");
 		this.id = id;
 		this.parentId = parentId;
@@ -60,6 +75,49 @@ public class Category {
 	public int hashCode() {
 		final int prime = 16;
 		return name.hashCode()*prime;
+	}		
+	
+	/**Loads list of categories from server ordered by category id
+	 * @throws DataLoadException 
+	 **/
+	public static List<Category> load() throws DataLoadException {		
+		Document doc = DataLoader.load(getRequest());
+		try {
+			return parse(doc);
+		} catch (XPathExpressionException e) {
+			throw new DataLoadException(e);
+		}
+	}
+	
+	private static QRequest getRequest() {
+		QRequest req = new QRequest(QRequest.Type.SELECT);
+		req.addQuery("SELECT * FROM categories ORDER BY id;");
+		return req;
+	}
+	
+	private static List<Category> parse(Document doc) throws XPathExpressionException {
+		List<Category> cat = new ArrayList<Category>();
+		XPathFactory xpfactory = XPathFactory.newInstance();
+		XPath xpath = xpfactory.newXPath();
+		NodeList nodes = (NodeList)xpath.evaluate("result/line", doc, XPathConstants.NODESET);
+		for(int i = 0; i< nodes.getLength(); i++) {
+			Node n = nodes.item(i);
+			//must be unique and not null
+			int id = Integer.valueOf(xpath.evaluate("ID", n));
+			//can be null, so..
+			int parentId;
+			try{
+				parentId = Integer.valueOf(xpath.evaluate("PARENT_ID", n));			
+			} catch (NumberFormatException e) {
+				parentId = 0;
+			}
+			Category c = new Category(id,
+					parentId,
+					xpath.evaluate("NAME", n),
+					xpath.evaluate("DESCRIPTION", n));
+			cat.add(c);
+		}
+		return cat;
 	}
 	
 	@Override
@@ -76,4 +134,18 @@ public class Category {
 			return false;
 		return true;
 	}
+	
+	//TODO:remove
+	public static void main(String[] args) throws Exception {
+		QServer.start();
+		List<Category> cat = Category.load();
+	
+		for(Category c:cat) {
+			System.out.println(c);
+		}
+		cat = Category.load();
+		QServer.stop();
+	}
+	
+
 }
