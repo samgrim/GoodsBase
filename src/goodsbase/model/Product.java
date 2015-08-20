fb3c89863cfb054a6f0a3092cd3293caf1014e34
samgrim <ds.lagrim@gmail.com>
2015-08-20 13:08:24 +0300

    add db methods and optimize some others 	modified:   src/goodsbase/model/Product.java

--- src/goodsbase/model/Product.java
+++ src/goodsbase/model/Product.java
@@ -1,25 +1,43 @@
 package goodsbase.model;
 
+import java.util.HashSet;
+import java.util.Set;
 
-/**Describes good*/
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import goodsbase.qserver.QRequest;
+
+
+/**Describes product*/
 public class Product {
 
+	private int id;
 	private String name;
 	private String description;
-	private Manufacturer manufacturer;
+	private String manufacturer;
 	private Category category;
 	private String tradeMark;
 	
 	/**@throws NullPointerException if name, manufacturer or category is null*/
-	public Product(String name, String tradeMark, Manufacturer manufacturer, Category category) {
-		if(name == null) throw new  NullPointerException("Name can't be null");
-		if(manufacturer == null) throw new  NullPointerException("Manufacturer can't be null");
-		if(category == null) throw new  NullPointerException("Category can't be null");
-		this.name = name;
-		this.manufacturer = manufacturer;
-		this.category = category;
-		/*tradeMark can be null*/
+	public Product(String name, String description, String tradeMark, String manufacturer, Category category) {
+		this.setName(name);
+		this.setDescription(description);
 		this.setTradeMark(tradeMark);
+		this.setManufacturer(manufacturer);
+		this.setCategory(category);
+	}
+	
+	private Product(int id, String name, String description, String tradeMark,
+						String manufacturer, Category category) {
+		this(name, description, tradeMark, manufacturer, category);
+		this.id = id;
 	}
 
 	public String getName() {
@@ -40,12 +58,12 @@ public class Product {
 		this.description = description;
 	}
 
-	public Manufacturer getManufacturer() {
+	public String getManufacturer() {
 		return manufacturer;
 	}
 
 	/**@throws NullPointerException if manufacturer is null*/
-	public void setManufacturer(Manufacturer manufacturer) {
+	public void setManufacturer(String manufacturer) {
 		if(manufacturer == null) throw new  NullPointerException("Manufacturer can't be null");
 		this.manufacturer = manufacturer;
 	}
@@ -66,6 +84,13 @@ public class Product {
 
 	public void setTradeMark(String tradeMark) {
 		this.tradeMark = tradeMark;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
 	}
 	
 	/**@return name + trade mark*/
@@ -113,7 +138,106 @@ public class Product {
 		return true;
 	}
 	
+	/**Loads set of products of the specified category
+	 * from server database
+	 * @throws DataLoadException 
+	 **/
+	public static Set<Product> load(Category c) throws DataLoadException {		
+		Document doc = DataLoader.load(getSelectRequest(c));
+		try {
+			return parse(doc, c);
+		} catch (XPathExpressionException e) {
+			throw new DataLoadException(e);
+		}
+	}
+	/**Removes product from the server database
+	 * @throws DataLoadException 
+	 **/
+	//TODO: constraints
+	public static boolean delete(Product prod) throws DataLoadException {
+		int res = DataLoader.execute(getDeleteRequest(prod));
+		if (res == QRequest.OK_CODE)
+			return true;
+		return false;
+	}
 	
+
+
+	/**Updates category in the server database
+	 * @throws DataLoadException 
+	 **/
+	public static boolean update(Product prod) throws DataLoadException {
+		int res = DataLoader.execute(getUpdateRequest(prod));
+		if (res == QRequest.OK_CODE)
+			return true;
+		return false;
+	}
+	
+	public static boolean insert(Product prod) throws DataLoadException {
+		int res = DataLoader.execute(getInsertRequest(prod));
+		if (res == QRequest.OK_CODE)
+			return true;
+		return false;
+	}
+	
+	private static QRequest getSelectRequest(Category cat) {
+		QRequest req = new QRequest(QRequest.Type.SELECT);
+		req.addQuery("SELECT id, name, description, trade_mark,"
+				+ "manufacturer FROM products WHERE category_id =" + cat.getId() + ";");
+		return req;
+	}
+	
+	private static QRequest getInsertRequest(Product prod) {
+		QRequest req = new QRequest(QRequest.Type.UPDATE);
+		req.addQuery("INSERT INTO products (name, description, trade_mark,"
+				+ "manufacturer, category_id) VALUES ( '" + prod.getName() + "','"
+						+ prod.getDescription() +"','"
+						+ prod.getTradeMark() +"','"
+						+ prod.getManufacturer() +"','"
+						+ prod.getCategory().getId() +"');");
+		return req;
+	}
+	
+	private static QRequest getUpdateRequest(Product prod) {
+		QRequest req = new QRequest(QRequest.Type.UPDATE);
+		req.addQuery("UPDATE products SET name ='" + prod.getName() 
+				+ "', description ='" + prod.getDescription()
+				+"', trade_mark = '" + prod.getTradeMark()
+				+"', manufacturer = '" + prod.getManufacturer()
+				+"', category_id = '" + prod.getCategory().getId()
+				+"' WHERE id = "+prod.getId()+";");
+		return req;
+	}
+	
+	private static QRequest getDeleteRequest(Product prod) {
+		QRequest req = new QRequest(QRequest.Type.UPDATE);
+		req.addQuery("DELETE FROM products WHERE id = " + prod.getId() + ";");
+		return req;
+	}
+	
+	
+	private static Set<Product> parse(Document doc, Category cat) throws XPathExpressionException {
+		Set<Product> prod = new HashSet<Product>();
+		XPathFactory xpfactory = XPathFactory.newInstance();
+		XPath xpath = xpfactory.newXPath();
+		NodeList nodes = (NodeList)xpath.evaluate("result/line", doc, XPathConstants.NODESET);
+		for(int i = 0; i< nodes.getLength(); i++) {
+			Node n = nodes.item(i);
+			//must be unique and not null
+			int id = Integer.valueOf(xpath.evaluate("ID", n));
+			//can be null, so..
+			
+			Product p = new Product(id,
+					xpath.evaluate("NAME", n),
+					xpath.evaluate("DESCRIPTION", n),
+					xpath.evaluate("TRADE_MARK", n),
+					xpath.evaluate("MANUFACTURER", n),
+					cat);
+			prod.add(p);
+		}
+		return prod;
+	}
+
 	
 	
 }
